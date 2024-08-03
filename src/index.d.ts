c68cb2b7a4af84395d205c333d337e762e49a32e
Emile Fugulin <code@efugulin.com>
2024-08-03 08:50:59 -0400

    Switch to shared package

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
 ///<reference path="types/runtime.d.ts" />
 ///<reference path="types/common.d.ts" />
 ///<reference path="types/typing.d.ts" />
-///<reference path="engine/index.d.ts" />
+///<reference types="@caido/quickjs-types" />
